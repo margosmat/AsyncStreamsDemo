b9ad53dc0fd26228d46b214459c4379d22cf64cd
margosmat <mateuszmargos@gmail.com>
2019-02-28 20:03:47 +0100

    Add project files.

---
+++ AsyncStreamsDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AsyncStreamsDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var names = GetNames();
+
+            await foreach(var name in names)
+            {
+                Console.WriteLine($"{name}");
+            }
+        }
+
+        private static async IAsyncEnumerable<string> GetNames()
+        {
+            var names = new[] { "John Doe", "Jan Kowalski", "Piotr Nowak", "Lech Roch Pawlak", "Tiger Bonzo" };
+
+            foreach(var name in names)
+            {
+                await Task.Delay(1000);
+                yield return name;
+            }
+        }
+    }
+}
